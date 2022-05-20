f5a2ec27f85bbd31f9f3e85b07af07208305ad12
Ricardo Margalhau <suchxploit@gmail.com>
2022-05-20 21:51:01 +0100

    adds missing annotations for wireless device interface

--- src/dbus/device/nm_device_wireless.rs
+++ src/dbus/device/nm_device_wireless.rs
@@ -15,17 +15,24 @@ pub trait NetworkManagerWirelessDevice {
     #[dbus_proxy(signal)]
     fn access_point_added(&self, access_point: ObjectPath<'_>) -> zbus::Result<()>;
 
+    #[dbus_proxy(signal)]
     fn access_point_removed(&self, access_point: ObjectPath<'_>) -> zbus::Result<()>;
 
+    #[dbus_proxy(property)]
     fn perm_hw_address(&self) -> zbus::Result<String>;
 
+    #[dbus_proxy(property)]
     fn mode(&self) -> zbus::Result<u32>;
 
+    #[dbus_proxy(property)]
     fn bitrate(&self) -> zbus::Result<u32>;
 
+    #[dbus_proxy(property)]
     fn active_access_point(&self) -> zbus::Result<OwnedObjectPath>;
 
+    #[dbus_proxy(property)]
     fn wireless_capabilities(&self) -> zbus::Result<u32>;
 
+    #[dbus_proxy(property)]
     fn last_scan(&self) -> zbus::Result<i64>;
 }
